fcee788c41ffb435fe1acc83654910b78eed1c00
Vaibhav Ramchandra Desai <127377578+Vaibhavdesai99@users.noreply.github.com>
2023-09-07 20:13:59 +0530

    Comment Added

--- src/Components/Sort-Salary/Salary.js
+++ src/Components/Sort-Salary/Salary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import RenderList from "./RenderList";
+// Sorting Data on the basis of salary.
 
 let Data = [
   { id: 1, name: "Akash", surName: "desai", salary: 100000 },
@@ -11,6 +12,7 @@ const Salary = () => {
 
   const sortedData = userData.sort((a, b) => b.salary - a.salary);
 
+  // Deleting the specifc item with the help of id : lifting the state up.
   const filterDataonDelete = (incomingId) => {
     const deleteFilter = userData.filter((item) => item.id !== incomingId);
 
